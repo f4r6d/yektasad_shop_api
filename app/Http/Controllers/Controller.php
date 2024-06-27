84eae9e50f59f548700ed1853f331dce5cf441b1
Farshid Gholizadeh <farshidelf@yahoo.com>
2024-06-27 12:18:23 +0330

    l5-swagger and laravel api package install

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,23 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="YektaShop",
+ *      description="YektaShop API Docs",
+ *      @OA\Contact(
+ *          email="farshidelf@yahoo.com"
+ *      )
+ * ),
+ * @OA\SecurityScheme(
+ *         type="apiKey",
+ *         description="Bearer {token}",
+ *         name="Authorization",
+ *         in="header",
+ *         securityScheme="api_token"
+ *     )
+ */
 abstract class Controller
 {
     //
